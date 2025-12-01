aaba4d24b9b4c739b33194a0ac662c1b5b614f1c
KenMirwell <kenmirwell@gmail.com>
2025-12-01 15:33:41 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
 <div>
     <h1 class="text-red-500">Hello</h1>
+    <h2>This should be green from custom CSS</h2>
 </div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
