4b1e64eda12718512ca852c3881a11eb07e68704
KenMirwell <kenmirwell@gmail.com>
2025-12-12 11:48:34 +0800

    update

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="bg-[#151517]">
+<div class="bg-[#19140e]">
     <?php get_template_part("page-sections/front-page/hero-section"); ?>
 </div>
 
--- page-sections/front-page/hero-section.php
+++ page-sections/front-page/hero-section.php
@@ -2,9 +2,9 @@
     <div class="flex flex-col justify-center p-[50px] items-center">
         <div class="">
             <h1 class="text-[72px] text-center font-bold">Remote Teams. <span class="text-[#ffffff]">Expert Talent. </span>Real Results.</h1>
-            <p class="text-[#ffffff] text-center">With 20+ years in Architecture, Engineering, and Creative fields, we connect top talent with firms. Our remote staffing solutions offer efficient, cost-effective production expansion through expert recruitment and dedicated management.</p>
+            <p class="text-[#a39e96] text-center">With 20+ years in Architecture, Engineering, and Creative fields, we connect top talent with firms. Our remote staffing solutions offer efficient, cost-effective production expansion through expert recruitment and dedicated management.</p>
         </div>
-        <div class="flex justify-between gap-[20px] pt-[20px]">
+        <div class="flex justify-between gap-[20px] pt-[50px]">
             <a class="gold-button" href="">Browse All Talents</a>
             <a class="border-[1px] gold-border gold-text" href="">How it Works</a>
         </div>
