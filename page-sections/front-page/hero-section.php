27e56787e38b1b742ddedeb759ba1067542ee96b
KenMirwell <kenmirwell@gmail.com>
2025-12-12 11:56:32 +0800

    update

--- page-sections/front-page/hero-section.php
+++ page-sections/front-page/hero-section.php
@@ -1,6 +1,6 @@
 <div class="custom-container px-[50px] py-[80px]">
     <div class="flex flex-col justify-center p-[50px] items-center">
-        <div class="">
+        <div class="border-b-[1px] border-[#6b6760]">
             <h1 class="text-[72px] text-center font-bold">Remote Teams. <span class="text-[#ffffff]">Expert Talent. </span>Real Results.</h1>
             <p class="text-[#a39e96] text-center">With 20+ years in Architecture, Engineering, and Creative fields, we connect top talent with firms. Our remote staffing solutions offer efficient, cost-effective production expansion through expert recruitment and dedicated management.</p>
         </div>
@@ -8,7 +8,7 @@
             <a class="gold-button" href="">Browse All Talents</a>
             <a class="border-[1px] gold-border gold-text" href="">How it Works</a>
         </div>
-        <div class="flex justify-between gap-[50px] pt-[50px] border border-t-[1px] border-[#6b6760]">
+        <div class="flex justify-between gap-[50px] pt-[50px]">
             <div class="flex flex-col justify-center items-center gap-[10px]">
                 <h6 class="text-[24px] font-bold">500+</h6>
                 <p class="text-[#6b6760]">Professionals</p>
