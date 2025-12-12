b465893e5dde71385e59a51c45294b40a7168fa2
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:58:00 +0800

    update

--- page-sections/front-page/choose-platform.php
+++ page-sections/front-page/choose-platform.php
@@ -5,19 +5,19 @@
                 <p class="text-[#a39e96] text-center">Unlock global expertise and boost your team’s potential with remote talent.</p>
         </div>
         <div class="flex justify-between gap-[20px]">
-            <div>
+            <div class="flex flex-col gap-[10px]">
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
             </div>
-            <div>
+            <div class="flex flex-col gap-[10px]">
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
             </div>
-            <div>
+            <div class="flex flex-col gap-[10px]">
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
             </div>
-            <div>
+            <div class="flex flex-col gap-[10px]">
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
             </div>
