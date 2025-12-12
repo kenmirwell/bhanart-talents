fabd37667b0815fa002910d81741d4645506df92
KenMirwell <kenmirwell@gmail.com>
2025-12-12 16:20:42 +0800

    update

--- page-sections/about-us/hero-section.php
+++ page-sections/about-us/hero-section.php
@@ -3,22 +3,11 @@
         <div class="flex flex-col justify-center p-[50px] items-center">
             <div class="flex flex-col justify-center items-center border-b-[1px] border-[#2d2820] pb-[50px]">
                 <div>
-                    <h1 class="text-[72px] text-center font-bold gold-text">Remote Teams. <span class="text-[#ffffff]">Expert Talent. </span>Real Results.</h1>
-                    <p class="text-[#a39e96] text-center">With 20+ years in Architecture, Engineering, and Creative fields, we connect top talent with firms. Our remote staffing solutions offer efficient, cost-effective production expansion through expert recruitment and dedicated management.</p>
+                    <h1 class="text-[72px] text-center font-bold gold-text">We are your Expanded Team!</h1>
+                    <p class="text-[#a39e96] text-center">Our team of creative professionals delivers top-notch services with artistic flair and problem-solving expertise. With years of industry experience, we tackle projects of any size, continuously honing our skills to produce exceptional, international-standard results.</p>
                 </div>
                 <div class="flex justify-between gap-[50px] pt-[50px]">
                     <a class="gold-button" href="">Browse All Talents</a>
-                    <a class="border-[1px] gold-border gold-text" href="">How it Works</a>
-                </div>
-            </div>
-            <div class="flex justify-between gap-[50px] pt-[50px]">
-                <div class="flex flex-col justify-center items-center gap-[10px]">
-                    <h6 class="text-[42px] font-bold gold-text">500+</h6>
-                    <p class="text-[#6b6760]">Professionals</p>
-                </div>
-                <div class="flex flex-col justify-center items-center gap-[10px]">
-                    <h6 class="text-[42px] font-bold gold-text">98%</h6>
-                    <p class="text-[#6b6760]">Satisfaction</p>
                 </div>
             </div>
         </div>
---
+++ page-sections/about-us/how-it-works.php
@@ -0,0 +1,42 @@
+<div class="bg-[#0d0700]">
+    <div class="custom-container px-[50px] py-[80px]">
+        <div class="flex flex-col justify-center items-center pb-[20px]">
+                <h2 class="gold-text text-[42px] text-center">How it Works</h2>
+        </div>
+        <div class="">
+            <div class="flex justify-between gap-[20px]">
+                <div>
+                    <h3 class="text-[24px] font-bold gold-text">Talent Assignment and Onboarding</h3>
+                    <p class="text-[#a39e96]">BhanArt Studio carefully selects and assigns the right professional to your project based on expertise and availability. We oversee the entire onboarding process, enabling a seamless transition from planning to execution. Once the professional are fully prepared to contribute effectively, our quality results begin immediately.</p>
+                </div>
+                <div>
+                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                        <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step1.jpeg" alt="Marcus Johnson">
+                    </div>
+                </div>
+            </div>
+             <div class="flex justify-between gap-[20px]">
+                <div>
+                    <h3 class="text-[24px] font-bold gold-text">Comprehensive Management and Support</h3>
+                    <p class="text-[#a39e96]">We manage all administrative responsibilities—payroll processing, and up-to-front scheduling. Enjoying the priceless team, satisfaction for building the job requirements. Balancing excellent results, by rapidly these tools, we allow you to employees on your project goals without the distraction of day-to-day management of your team.</p>
+                </div>
+                <div>
+                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                        <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step2.jpeg" alt="Marcus Johnson">
+                    </div>
+                </div>
+            </div>
+             <div class="flex justify-between gap-[20px]">
+                <div>
+                    <h3 class="text-[24px] font-bold gold-text">Seamless Client-Remote Team Collaboration</h3>
+                    <p class="text-[#a39e96]">We facilitate direct communication with your remote team allowing for real-time collaboration. This allows for immediate real-time updates and immediate feedback ensuring that project stays on track and aligned with your objectives. While enhancing overall efficiency and responsiveness, it enhances collaboration and removes communication barriers that are your goals.</p>
+                </div>
+                <div>
+                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                        <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step3.jpeg" alt="Marcus Johnson">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
