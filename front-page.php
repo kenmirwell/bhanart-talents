986a1f79a36d46e707ae83c716b83e16f9b6267f
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:30:02 +0800

    update

--- front-page.php
+++ front-page.php
@@ -2,6 +2,7 @@
 
 <div class="bg-[#19140e]">
     <?php get_template_part("page-sections/front-page/hero-section"); ?>
+    <?php get_template_part("page-sections/front-page/featured-categories"); ?>
 </div>
 
 <?php get_footer(); ?>
