3f1012c3467107bc4f5c1b23fc4cdc615bad9170
KenMirwell <kenmirwell@gmail.com>
2025-12-12 16:25:02 +0800

    update

--- page-sections/about-us/how-it-works.php
+++ page-sections/about-us/how-it-works.php
@@ -6,7 +6,7 @@
         <div class="">
             <div class="flex justify-between gap-[20px]">
                 <div>
-                    <h3 class="text-[24px] font-bold gold-text">Talent Assignment and Onboarding</h3>
+                    <h3 class="text-[24px] font-bold text-[#ffffff]">Talent Assignment and Onboarding</h3>
                     <p class="text-[#a39e96]">BhanArt Studio carefully selects and assigns the right professional to your project based on expertise and availability. We oversee the entire onboarding process, enabling a seamless transition from planning to execution. Once the professional are fully prepared to contribute effectively, our quality results begin immediately.</p>
                 </div>
                 <div>
@@ -17,7 +17,7 @@
             </div>
              <div class="flex justify-between gap-[20px]">
                 <div>
-                    <h3 class="text-[24px] font-bold gold-text">Comprehensive Management and Support</h3>
+                    <h3 class="text-[24px] font-boldtext-[#ffffff]">Comprehensive Management and Support</h3>
                     <p class="text-[#a39e96]">We manage all administrative responsibilities—payroll processing, and up-to-front scheduling. Enjoying the priceless team, satisfaction for building the job requirements. Balancing excellent results, by rapidly these tools, we allow you to employees on your project goals without the distraction of day-to-day management of your team.</p>
                 </div>
                 <div>
@@ -28,7 +28,7 @@
             </div>
              <div class="flex justify-between gap-[20px]">
                 <div>
-                    <h3 class="text-[24px] font-bold gold-text">Seamless Client-Remote Team Collaboration</h3>
+                    <h3 class="text-[24px] font-boldtext-[#ffffff]">Seamless Client-Remote Team Collaboration</h3>
                     <p class="text-[#a39e96]">We facilitate direct communication with your remote team allowing for real-time collaboration. This allows for immediate real-time updates and immediate feedback ensuring that project stays on track and aligned with your objectives. While enhancing overall efficiency and responsiveness, it enhances collaboration and removes communication barriers that are your goals.</p>
                 </div>
                 <div>
