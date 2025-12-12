090a01242c7dba0eb2f9b546b9cb7002f31f46fa
KenMirwell <kenmirwell@gmail.com>
2025-12-12 13:42:39 +0800

    update

--- page-sections/front-page/choose-platform.php
+++ page-sections/front-page/choose-platform.php
@@ -5,7 +5,7 @@
                 <p class="text-[#a39e96] text-center">Unlock global expertise and boost your team’s potential with remote talent.</p>
         </div>
         <div class="flex justify-between gap-[20px]">
-            <div class="flex flex-col gap-[10px]">
+            <div class="flex flex-col gap-[20px]">
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
             </div>
