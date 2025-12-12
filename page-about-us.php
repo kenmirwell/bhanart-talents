86d71e3f41fcd0f604b0e7316a3ad15b41a91659
KenMirwell <kenmirwell@gmail.com>
2025-12-12 16:21:41 +0800

    update

--- page-about-us.php
+++ page-about-us.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
 <?php get_template_part("page-sections/about-us/hero-section"); ?>
+<?php get_template_part("page-sections/about-us/how-it-works"); ?>
 
 <?php get_footer(); ?>
