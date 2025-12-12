2e0d05dd231e03c56d57ee3a1a3c833a819fd442
KenMirwell <kenmirwell@gmail.com>
2025-12-12 14:18:17 +0800

    update

---
+++ page-sections/front-page/featured-talents.php
@@ -0,0 +1,24 @@
+<div class="bg-white">
+    <div class="custom-container px-[50px] py-[80px]">
+        <div class="flex flex-col justify-center items-center pb-[20px]">
+                <h2 class="gold-text text-[42px] text-center">Featured Talents</h2>
+                <p class="text-[#a39e96] text-center">Access to top talent from around the globe at your fingertips, showcasing the effectiveness of remote hiring.</p>
+                <a class="gold-button text-center" href="">Browse All Talents</a>
+        </div>
+        <div class="p-[20px] rounded-2xl border-[1px] border-[#2d2820]">
+            <div>
+                <img src="./assets/guy1.jpeg" alt="">
+            </div>
+            <div class="flex">
+                <h2 class="text-[24px] text-white">Marcus Johnson</h2>
+                <p class="gold-text">3D Artist</p>
+            </div>
+            <div>
+                <div class="py-[20px] px-[8px] rounded-full">
+                    <p y=text-[#a39e96]>3D Modeling</p>
+                </div>
+            </div>
+            <a class="gold-button" href="">View Profile</a>
+        </div>
+    </div>
+</div>
