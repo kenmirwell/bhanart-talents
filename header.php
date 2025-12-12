19cecf92f734486ca09772438d3bae824b6afddb
KenMirwell <kenmirwell@gmail.com>
2025-12-12 09:22:45 +0800

    update

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
 
 <body>
-  <header id="header" class="bg-white shadow-md w-[100%] transition-all duration-200 ease">
+  <header id="header" class="shadow-md w-[100%] transition-all duration-200 ease">
     <div class="w-[1200px] mx-auto flex items-center justify-between py-[10px]">
         <h1>THIS IS The HEADER</h1>
     </div>
