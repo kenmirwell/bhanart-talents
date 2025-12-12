40313084fd853465245263729aeddf36dd3ab5c3
KenMirwell <kenmirwell@gmail.com>
2025-12-12 09:49:52 +0800

    update

--- header.php
+++ header.php
@@ -18,14 +18,14 @@
   <header id="header" class="shadow-md w-[100%] transition-all duration-200 ease">
     <div class="w-[1200px] mx-auto flex items-center justify-between py-[10px]">
         <div>
-          <h1>BhanartTalents</h1>
+          <h1 class="font-bold">BhanartTalents</h1>
         </div>
-        <div>
-          <a href="">Home</a>
-          <a href="">About Us</a>
-          <a href="">Our Talents</a>
+        <div class="text-[#ffffff]">
+          <a href="/">Home</a>
+          <a href="/">About Us</a>
+          <a href="/">Our Talents</a>
         </div>
-        <div>Hire Talents</div>
+        <a class=" py-[15px] px-[20px] bg-[#e7b20d] rounded-xl" href="/">Hire Talents</a>
     </div>
   </header>
 
