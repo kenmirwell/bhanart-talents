4e759bcf78331ac7ef845892531c4b6243810a6c
KenMirwell <kenmirwell@gmail.com>
2025-12-12 14:32:18 +0800

    update

--- page-sections/front-page/featured-talents.php
+++ page-sections/front-page/featured-talents.php
@@ -7,7 +7,7 @@
         </div>
         <div class="flex flex-col rounded-2xl border-[1px] border-[#2d2820]">
             <div>
-                <img src="../../assets/guy1.jpeg" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/guy1.jpeg" alt="Marcus Johnson">
             </div>
             <div class="bg-[#0d0700] p-[20px]">
                 <div class="flex flex-col">
