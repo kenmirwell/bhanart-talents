cbd7c684a97e73fc0f540a122735ff8bdf364f61
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:33:33 +0800

    update

--- page-sections/front-page/featured-categories.php
+++ page-sections/front-page/featured-categories.php
@@ -5,7 +5,7 @@
         <a class="gold-button" href="">See All Categories</a>
    </div>
    <div class="">
-        <div class="bg-[#12100C] p-[20px] rounded-[10px] border border-[1px] border-[#2d2820] hover:border-[#e7b20d] drop-shadow-[0_4px_6px_#2d2820]">
+        <div class="bg-[#12100C] p-[20px] rounded-[10px] border border-[1px] border-[#2d2820] hover:border-[#e7b20d] shadow-[0_4px_6px_rgba(231,178,13,0)] hover:shadow-[0_4px_6px_rgba(231,178,13,0.5)]">
             <div>
                 <h3>2D & 3D Artist</h3>
                 <p>From concept art to photorealistic renders, our artists bring visions to life with technical precision and creative excellence.</p>
