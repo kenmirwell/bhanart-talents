e1d7841ac878e902f17ec827d3714dd9edcce8a5
KenMirwell <kenmirwell@gmail.com>
2025-12-12 16:26:54 +0800

    update

--- page-sections/about-us/how-it-works.php
+++ page-sections/about-us/how-it-works.php
@@ -10,29 +10,29 @@
                     <p class="text-[#a39e96]">BhanArt Studio carefully selects and assigns the right professional to your project based on expertise and availability. We oversee the entire onboarding process, enabling a seamless transition from planning to execution. Once the professional are fully prepared to contribute effectively, our quality results begin immediately.</p>
                 </div>
                 <div>
-                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                    <div class="relative w-[300px] h-[280px] overflow-hidden">
                         <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step1.jpeg" alt="Marcus Johnson">
                     </div>
                 </div>
             </div>
              <div class="flex justify-between gap-[20px]">
                 <div>
-                    <h3 class="text-[24px] font-boldtext-[#ffffff]">Comprehensive Management and Support</h3>
+                    <h3 class="text-[24px] font-boldt ext-[#ffffff]">Comprehensive Management and Support</h3>
                     <p class="text-[#a39e96]">We manage all administrative responsibilities—payroll processing, and up-to-front scheduling. Enjoying the priceless team, satisfaction for building the job requirements. Balancing excellent results, by rapidly these tools, we allow you to employees on your project goals without the distraction of day-to-day management of your team.</p>
                 </div>
                 <div>
-                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                    <div class="relative w-[300px] h-[280px] overflow-hidden">
                         <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step2.jpeg" alt="Marcus Johnson">
                     </div>
                 </div>
             </div>
              <div class="flex justify-between gap-[20px]">
                 <div>
-                    <h3 class="text-[24px] font-boldtext-[#ffffff]">Seamless Client-Remote Team Collaboration</h3>
+                    <h3 class="text-[24px] font-bold text-[#ffffff]">Seamless Client-Remote Team Collaboration</h3>
                     <p class="text-[#a39e96]">We facilitate direct communication with your remote team allowing for real-time collaboration. This allows for immediate real-time updates and immediate feedback ensuring that project stays on track and aligned with your objectives. While enhancing overall efficiency and responsiveness, it enhances collaboration and removes communication barriers that are your goals.</p>
                 </div>
                 <div>
-                    <div class="relative w-[100%] h-[280px] overflow-hidden">
+                    <div class="relative w-[300px] h-[280px] overflow-hidden">
                         <img class="absolute w-[100%] h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/step3.jpeg" alt="Marcus Johnson">
                     </div>
                 </div>
