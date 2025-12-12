931b5a77c627cd174b75feea7554a81bb059a6f0
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:56:39 +0800

    update

--- page-sections/front-page/hero-section.php
+++ page-sections/front-page/hero-section.php
@@ -3,7 +3,7 @@
         <div class="flex flex-col justify-center p-[50px] items-center">
             <div class="flex flex-col justify-center items-center border-b-[1px] border-[#2d2820] pb-[50px]">
                 <div>
-                    <h1 class="text-[72px] text-center font-bold">Remote Teams. <span class="text-[#ffffff]">Expert Talent. </span>Real Results.</h1>
+                    <h1 class="text-[72px] text-center font-bold gold-text">Remote Teams. <span class="text-[#ffffff]">Expert Talent. </span>Real Results.</h1>
                     <p class="text-[#a39e96] text-center">With 20+ years in Architecture, Engineering, and Creative fields, we connect top talent with firms. Our remote staffing solutions offer efficient, cost-effective production expansion through expert recruitment and dedicated management.</p>
                 </div>
                 <div class="flex justify-between gap-[50px] pt-[50px]">
@@ -13,11 +13,11 @@
             </div>
             <div class="flex justify-between gap-[50px] pt-[50px]">
                 <div class="flex flex-col justify-center items-center gap-[10px]">
-                    <h6 class="text-[42px] font-bold">500+</h6>
+                    <h6 class="text-[42px] font-bold gold-text">500+</h6>
                     <p class="text-[#6b6760]">Professionals</p>
                 </div>
                 <div class="flex flex-col justify-center items-center gap-[10px]">
-                    <h6 class="text-[42px] font-bold">98%</h6>
+                    <h6 class="text-[42px] font-bold gold-text">98%</h6>
                     <p class="text-[#6b6760]">Satisfaction</p>
                 </div>
             </div>
