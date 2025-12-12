1ad6302a1ed36c0b12446b81c5a814586b60ad9d
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:53:42 +0800

    update

--- page-sections/front-page/choose-platform.php
+++ page-sections/front-page/choose-platform.php
@@ -4,7 +4,7 @@
                 <h2 class="gold-text text-[42px] text-center">Why Hire Remote Talents</h2>
                 <p class="text-[#a39e96] text-center">Unlock global expertise and boost your team’s potential with remote talent.</p>
         </div>
-        <div class="flex justify-between">
+        <div class="flex justify-between gap-[20px]">
             <div>
                 <h3 class="text-[24px] font-bold text-white">Cost-Efficient Growth</h3>
                 <p class="text-[#a39e96]">Expand your team and boost production while saving 25-40% on expenses, helping you remain competitive without the high costs of traditional expansion.</p>
