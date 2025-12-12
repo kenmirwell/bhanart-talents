dccf813fbafa4d78421aed1df091931b990c2abc
KenMirwell <kenmirwell@gmail.com>
2025-12-12 12:40:32 +0800

    update

--- page-sections/front-page/featured-categories.php
+++ page-sections/front-page/featured-categories.php
@@ -1,18 +1,18 @@
 <div class="custom-container px-[50px] py-[80px]">
-   <div>
-        <h2 class="gold-text text-[42px]">Featured Talent Categories</h2>
+   <div class="flex flex-col justify-center items-center pb-[20px]">
+        <h2 class="gold-text text-[42px] text-center">Featured Talent Categories</h2>
         <p class="text-[#a39e96] text-center">Find the perfect professional for your next project</p>
-        <a class="gold-button" href="">See All Categories</a>
+        <a class="gold-button text-center" href="">See All Categories</a>
    </div>
    <div class="">
-        <div class="bg-[#12100C] p-[20px] rounded-[10px] border border-[1px] border-[#2d2820] hover:border-[#e7b20d] shadow-[0_4px_6px_rgba(231,178,13,0)] hover:shadow-[0_4px_6px_rgba(231,178,13,0.5)]">
+        <div class="bg-[#12100C] p-[20px] rounded-[10px] border border-[1px] border-[#2d2820] hover:border-[#e7b20d] shadow-[0_4px_6px_rgba(231,178,13,0)] hover:shadow-[2px_6px_6px_rgba(231,178,13,0.5)]">
             <div>
-                <h3>2D & 3D Artist</h3>
-                <p>From concept art to photorealistic renders, our artists bring visions to life with technical precision and creative excellence.</p>
+                <h3 class="text-[24px] font-bold">2D & 3D Artist</h3>
+                <p class="text-[#a39e96]">From concept art to photorealistic renders, our artists bring visions to life with technical precision and creative excellence.</p>
             </div>
             <div>
-                <p>150+ professionals</p>
-                <a href="">Explore</a>
+                <p class="text-[#2d2820]">150+ professionals</p>
+                <a class="gold-text" href="">Explore</a>
             </div>
         </div>
    </div>
