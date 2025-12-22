b6240176c58fd95f7e91e5e3e2ec4c2690d4fd8c
KenMirwell <kenmirwell@gmail.com>
2025-12-22 11:50:45 +0800

    update

--- page-our-talents.php
+++ page-our-talents.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
 <?php get_template_part("page-sections/our-talents/hero-section"); ?>
+<?php get_template_part("page-sections/our-talents/search-section"); ?>
 
 <?php get_footer(); ?>
