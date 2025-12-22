51b4a12ba1eed23ac8e15113050982c1d8a89d1c
KenMirwell <kenmirwell@gmail.com>
2025-12-22 11:49:03 +0800

    update

--- page-sections/our-talents/hero-section.php
+++ page-sections/our-talents/hero-section.php
@@ -1,5 +1,5 @@
 <div class="our-talents-hero relative bg-[linear-gradient(135deg,rgb(13,7,0)_0%,rgb(26,21,16)_50%,rgb(13,7,0)_100%)]">
-    <div class="custom-container px-[50px] py-[80px]">
+    <div class="custom-container px-[50px] pt-[80px]">
         <div class="flex flex-col justify-center p-[50px] items-center">
             <div class="flex flex-col justify-center items-center border-b-[1px] border-[#2d2820] pb-[50px]">
                 <div>
