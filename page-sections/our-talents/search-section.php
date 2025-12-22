f40363f587a35ba2160a748a5d87bc0ade58fc9e
KenMirwell <kenmirwell@gmail.com>
2025-12-22 12:07:51 +0800

    update

--- page-sections/our-talents/search-section.php
+++ page-sections/our-talents/search-section.php
@@ -1,9 +1,9 @@
-<div class="custom-container">
+<div class="custom-container overflow-y-scroll">
     <div class="flex gap-[20px] pb-[20px]">
         <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] w-[100%]">
-            <input class="p-[20px] text-[#a39e96]" type="text" placeholder="Search">
+            <input class="px-[12px] py-[8px] text-[#a39e96]" type="text" placeholder="Search">
         </div>
-        <button class="gold-button">Search</button>
+        <button class="bg-[#e7b20d] px-[40px] py-[10px]">Search</button>
     </div>
     <div>
         <div class="flex gap-[20px]">
