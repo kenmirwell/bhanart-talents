9bce86e0d34ff06f15f966b80c1b0f809e114b6a
KenMirwell <kenmirwell@gmail.com>
2025-12-22 11:59:19 +0800

    update

--- page-our-talents.php
+++ page-our-talents.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="bg-[linear-gradient(135deg,rgb(13,7,0)_0%,rgb(26,21,16)_50%,rgb(13,7,0)_100%)]">
+<div class="bg-[linear-gradient(135deg,rgb(13,7,0)_0%,rgb(26,21,16)_50%,rgb(13,7,0)_100%)] pb-[80px]">
     <?php get_template_part("page-sections/our-talents/hero-section"); ?>
     <?php get_template_part("page-sections/our-talents/search-section"); ?>
 </div>
--- page-sections/our-talents/search-section.php
+++ page-sections/our-talents/search-section.php
@@ -1,5 +1,5 @@
-<div>
-    <div>
+<div class="custom-container">
+    <div class="flex gap-[20px]">
         <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96]">
             <input class="p-[20px] text-[#a39e96]" type="text" placeholder="Search">
         </div>
@@ -7,37 +7,37 @@
     </div>
     <div>
         <div class="flex gap-[20px]">
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>All</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Digital Artist</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>3D Artist</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Graphic Designer</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Illustrator</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Motion Designer</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Photographer</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Art Director</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Ui/UX Designer</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>3D Modeler</p>
             </div>
-            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[5px] py-[10px]">
+            <div class="border-[1px] border-[#3a352d] rounded-[10px] text-[#a39e96] px-[12px] py-[8px]">
                 <p>Virtual Assitant</p>
             </div>
         </div>
